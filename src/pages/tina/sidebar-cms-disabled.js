89bd019c4b0e76d27cd14b799865c055562b077d
Mitch MacKenzie <mitch@forestry.io>
2020-12-14 12:58:26 -0400

    Sidebar with cms disabled

---
+++ src/pages/tina/sidebar-cms-disabled.js
@@ -0,0 +1,28 @@
+import { withTina, useForm, usePlugin, useCMS } from 'tinacms'
+
+function Index() {
+    const cms = useCMS();
+    const formConfig = {
+        id: 'editor',
+        initialValues: {
+            text: 'hello!'
+        },
+        fields: [
+            {
+                name: 'text',
+                label: 'Text',
+                component: 'textarea'
+            }
+        ],
+    };
+
+    const [modifiedValues, form] = useForm(formConfig);
+    usePlugin(form);
+    return <p>{modifiedValues.text}</p>
+}
+
+export default withTina(Index, {
+    enabled: false,
+    sidebar: true,
+    toolbar: true
+})
--- src/pages/tina/sidebar-dynamic-wysiwyg.js
+++ src/pages/tina/sidebar-dynamic-wysiwyg.js
@@ -1,3 +1,34 @@
-export default function Index() {
-    return <p>todo</p>
+import { withTina, useForm, usePlugin, useCMS } from 'tinacms'
+import { MarkdownFieldPlugin } from 'react-tinacms-editor'
+
+function Index() {
+    const cms = useCMS();
+    //@TODO: make dynamic
+    cms.plugins.add(MarkdownFieldPlugin);
+
+    const formConfig = {
+        id: 'editor',
+        initialValues: {
+            text: 'hello!'
+        },
+        fields: [
+            {
+                name: 'text',
+                label: 'Text',
+                component: 'markdown'
+            }
+        ],
+    };
+
+    const [modifiedValues, form] = useForm(formConfig);
+    usePlugin(form);
+
+    // should render markdown
+    return <p>{modifiedValues.text}</p>
 }
+
+export default withTina(Index, {
+    enabled: true,
+    sidebar: true,
+    toolbar: true
+})
