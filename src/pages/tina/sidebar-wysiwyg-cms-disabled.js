f74b2a22a22d9d7934142f83156c8df0ab50f626
Mitch MacKenzie <mitch@forestry.io>
2020-12-14 14:18:48 -0400

    Sidebar wysiwyg cms disabled

---
+++ src/pages/tina/sidebar-wysiwyg-cms-disabled.js
@@ -0,0 +1,33 @@
+import { withTina, useForm, usePlugin, useCMS } from 'tinacms'
+import { MarkdownFieldPlugin } from 'react-tinacms-editor'
+
+function Index() {
+    const cms = useCMS();
+    cms.plugins.add(MarkdownFieldPlugin);
+
+    const formConfig = {
+        id: 'editor',
+        initialValues: {
+            text: 'hello!'
+        },
+        fields: [
+            {
+                name: 'text',
+                label: 'Text',
+                component: 'markdown'
+            }
+        ],
+    };
+
+    const [modifiedValues, form] = useForm(formConfig);
+    usePlugin(form);
+
+    // should render markdown
+    return <p>{modifiedValues.text}</p>
+}
+
+export default withTina(Index, {
+    enabled: false,
+    sidebar: true,
+    toolbar: true
+})
